a685507032cbbb31d8781441539f815d38c74c72
Tom <tomatkinsin@gmail.com>
2020-04-08 03:08:51 +0000

    better testing system

--- test.py
+++ test.py
@@ -3,9 +3,30 @@
 import unittest
 import main
 
-class testFuncs(unittest.TestCase):
-    def addTest(self):
-        self.assertEqual(add(3, 4), 7)
+if __name__ == "__main__":
+    testcount = 0
+    try:
+        # test 1
+        testcount += 1
+        assert main.add(1, 2) == 3
+        print("Success, add(1, 2) == 3")
+        
+        # test 2
+        testcount += 1
+        assert main.add(1, 4) != 3
+        print("Success, add(1, 4) != 3")
+
+        # test 3
+        testcount += 1
+        assert main.square(3) == 9
+        print("Success, square(3) == 9")
+
+        # test 4
+        testcount += 1
+        assert main.square(3) != 8
+        print("Success, square(3) != 8")
+
+    except:
+        #catch
+        print("Failed test %d." % testcount)
     
-    def squareTest(self):
-        self.assertEqual(square(3), 9)
