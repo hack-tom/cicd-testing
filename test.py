8327aa03daa019fb47af8a3d1738906782b62312
Tom <tomatkinsin@gmail.com>
2020-04-08 03:57:19 +0000

    Removed try/excepts to force CI failure when asserts fail

--- test.py
+++ test.py
@@ -4,29 +4,18 @@ import unittest
 import main
 
 if __name__ == "__main__":
-    testcount = 0
-    try:
-        # test 1
-        testcount += 1
-        assert main.add(1, 2) == 3
-        print("Success, add(1, 2) == 3")
-        
-        # test 2
-        testcount += 1
-        assert main.add(1, 4) != 3
-        print("Success, add(1, 4) != 3")
-
-        # test 3
-        testcount += 1
-        assert main.square(3) == 9
-        print("Success, square(3) == 9")
-
-        # test 4
-        testcount += 1
-        assert main.square(3) != 8
-        print("Success, square(3) != 8")
-
-    except:
-        #catch
-        print("Failed test %d." % testcount)
+    # test 1
+    assert main.add(1, 2) == 3
+    print("Success, add(1, 2) == 3")
     
+    # test 2
+    assert main.add(1, 4) != 3
+    print("Success, add(1, 4) != 3")
+
+    # test 3
+    assert main.square(3) == 9
+    print("Success, square(3) == 9")
+
+    # test 4
+    assert main.square(3) != 8
+    print("Success, square(3) != 8")
